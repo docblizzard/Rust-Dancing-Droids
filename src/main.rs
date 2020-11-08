029886124398a58f64b7bc95bc60263e1159bc6f
docblizzard <stephanelwin@hotmail.fr>
2020-11-08 20:58:46 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,15 +17,15 @@ struct Robot {
 }
 
 /// Instruction Donné
-
+/*
 enum Instruction {
     L,
     R,
     F
 }
-
+*/
 /// Possible Directions
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug)] /// Permet de comparer un enum Direction
 enum Direction {
     N,
     S,
@@ -35,7 +35,7 @@ enum Direction {
 
 
 /// Recupere une direction donné en char et renvoie en Enum Direction
-fn read_direction( d : char) -> Direction {
+fn read_direction( d : &char) -> Direction {
     match d {
         'N' => Direction:: N,
         'S' => Direction:: S,
@@ -45,43 +45,66 @@ fn read_direction( d : char) -> Direction {
     }
 }
 
+/// Test Limite du tableau Ne marche pas encore malheuresement
+fn check_limit(rbt: Robot) -> &i32 {
+    match rbt.direction {
+        Direction::N => match rbt.pos_Y {
+            0 => &rbt.pos_Y,
+            _ => &(&(rbt.pos_Y) +1),
+        },
+        Direction::E => match rbt.pos_X {
+            5 => &rbt.pos_X,
+            _ => &(&(rbt.pos_Y )+1),
+        },
+        Direction::S => match rbt.pos_Y {
+            5 => &rbt.pos_Y,
+            _ => &(&(rbt.pos_Y) +1),
+        },
+        _ => match rbt.pos_X {
+            0 => &rbt.pos_X,
+            _ => &(&(rbt.pos_Y) +1),
+        }
+    }
+}
 /// Instruction Donné selon la direction dans la quelle le robot regarde
 
-fn read_instruction(rbt : Robot, charac : char) -> Direction {
-    match charac {
+fn read_instruction(rbt : &Robot, chr : &char) -> Direction {
+    match chr {
         'L' => match rbt.direction {
             Direction::N => Direction::W,
             Direction::W => Direction::S,
             Direction::S => Direction::E,
             Direction::E => Direction::N,
         },
-        'R' => match rbt.direction {
+       'R' => match rbt.direction {
             Direction::N => Direction::E,
             Direction::E => Direction::S,
             Direction::S => Direction::W,
             Direction::W => Direction::N,
         },
-        _ => rbt.direction
+        _    => match rbt.direction {
+            Direction::E => Direction::E,
+            Direction::S => Direction::S,
+            Direction::W => Direction::W,
+            Direction::N => Direction::N,
+        }
     }
 }
 
 impl Robot {
-    fn Collision(&self,  other: &Robot) -> bool {
-        self.pos_X == other.pos_X || self.pos_Y == other.pos_Y
-    }
-
-    fn check_dir(&self) -> bool{
-        self.direction == Direction:: S
+    fn Collision(&self,  posX: i32, posY: i32) -> bool {
+        self.pos_X == posX && self.pos_Y == posY
     }
 
 }
 
-
+    /// Lis le fichier text et execute les differentes fonctions selon la ligne du fichier texte
 fn main() {
     let mut countchars = 0;
     let mut countlines = 0;
+
     let mut robot1 = Robot {
-        direction: Direction::W,
+        direction: Direction::S,
         pos_Y: 0,
         pos_X: 0
     };
@@ -93,26 +116,29 @@ fn main() {
     if robot1.check_dir() == true {
         robot1.pos_Y = robot1.pos_Y + 1;
     }
-        /// Lis le fichier text et execute les differentes fonctions selon la ligne
 
-    let mut text = File::open("src/text/two_robots.txt").expect("Open Failed");
-    let mut text = BufReader::new(text);
+
+    let  text = File::open("src/text/two_robots.txt").expect("Open Failed");
+    let  text = BufReader::new(text);
     for line in text.lines().filter_map(|result| result.ok()) {
+        println!("{}",countlines);
         for c in line.chars() {
             if countlines == 0 {
                 if c == 'N' || c == 'S' || c == 'W' || c == 'E' {
-                    robot1.direction = read_direction(c);
+                    robot1.direction = read_direction(&c);
                 }
-          /*  if countlines == 1{
+            }
+            if countlines == 1{
                 if c == 'L' || c == 'F' || c == 'R' {
-                    fn read_instruction(robot1, c : char);
+                    robot1.direction = read_instruction(&robot1, &c );
+                    if c == 'F' {
+                        robot1.pos_Y = check_limit(&robot1);
+                        robot1.pos_Y = check_limit(&robot1);
+                    }
                 }
-            } */
             }
         }
         countlines = countlines +1;
     }
-
-
-    println!("Robots colliding = {}, robot 1 in pos_Y = {:?}", robot1.Collision(&robot2), robot1.direction) ;
+        println!("robot 1 en robot x = {}, y = {}  in direction = {:?}",robot1.pos_X, robot1.pos_Y,  robot1.direction) ;
 }
